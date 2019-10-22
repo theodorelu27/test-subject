33497bcfe3c2b385e19c0ca44c5f90da2a5a828c
Theodore Lu <theodore.lumaksono@yahoo.com>
2019-10-23 00:55:57 +0200

    Update Landing Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
-  { path: '/', component: LandingComponent }
+  { path: '', component: LandingComponent },
 ];
 
 @NgModule({
